5e0b25f7065696d8af2f9caca52b5094c67f772b
TC <codebrotha1@gmail.com>
2021-05-24 18:33:27 -0500

    passed data from child to parent bottom-up with props

--- src/App.js
+++ src/App.js
@@ -25,9 +25,14 @@ const expenses = [
     date: new Date(2021, 5, 12),
   },
 ];
+
+  const addExpenseHandler = expense => {
+    console.log("IUn App.js");
+    console.log(expenses);
+  }
   return (
     <div>
-     <NewExpense/>
+     <NewExpense onAddExpense={addExpenseHandler}/>
      <Expenses items={expenses}/>
      </div>
   );
